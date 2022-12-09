77a3b7631c61197c9a63d106216075429c640c79
chenwei <Cw19950519>
2022-12-09 16:08:26 +0800

    修改BUG

--- src/main/java/com/growatt/test/TestCode.java
+++ src/main/java/com/growatt/test/TestCode.java
@@ -1,2 +1,10 @@
-package com.growatt.test;public class TestCode {
+package com.growatt.test;
+
+public class TestCode
+{
+    public static void main(String[] args)
+    {
+        int i = 10;
+        System.out.println("i = " + i);
+    }
 }
